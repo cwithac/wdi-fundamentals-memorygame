9d21bf36b0f2041feb0fe24f2caa2f2f3013f56e
Cathleen Wright <cathleenwright@Cathleens-MacBook-Pro.local>
2017-02-07 15:55:41 -0500

    boolean js added

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -3,3 +3,15 @@ var cardOne = "king";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "queen";
+
+if (cardOne === cardThree) {
+	alert("You found a match!");
+} else if (cardTwo === cardFour) {
+	alert("You found a match!");
+} else if (cardOne === cardTwo) {
+	alert("Sorry, try again.");
+} else if (cardThree === cardFour) {
+	alert("Sorry, try again."); 
+}
+
+
