e341445636343ddebdc60dbb465208838246e0b0
Cathleen Wright <cathleenwright@Cathleens-MacBook-Pro.local>
2017-02-16 15:20:42 -0500

    Unit 9 Assign

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,11 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+
+if (cardOne === cardTwo) {
+	//|| (cardThree === cardFour) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+};
+
