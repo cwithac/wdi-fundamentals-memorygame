915800e275240c48bda012bfd0e6733ac9037769
Cathleen Wright <cathleenwright@Cathleens-MacBook-Pro.local>
2017-02-17 12:41:46 -0500

    Unit 10 Assign

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,11 +3,20 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+var gameBoard = document.getElementById('game-board');
+function createCards() {
+for (var i = 0; i < 4; i++) {
+var newCard = document.createElement('div');
+newCard.className = 'card';
+gameBoard.appendChild(newCard);}
+}
 
-if (cardOne === cardTwo) {
+createCards();
+
+/*if (cardOne === cardTwo) {
 	//|| (cardThree === cardFour) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
-};
+};*/
 
