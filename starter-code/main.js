5b3d70b5954103ec53999ef36c5574416d509378
Cathleen Wright <cathleenwright@Cathleens-MacBook-Pro.local>
2017-02-17 20:13:21 -0500

    Unit 11 Assign

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,22 +1,43 @@
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
-var cardFour = "king";
+var cardFour = "king";*/
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
 var gameBoard = document.getElementById('game-board');
-function createCards() {
-for (var i = 0; i < 4; i++) {
-var newCard = document.createElement('div');
-newCard.className = 'card';
-gameBoard.appendChild(newCard);}
+	function createCards() {
+		for (var i = 0; i < cards.length; i++) {
+	var newCard = document.createElement('div');
+		newCard.setAttribute('data-card', cards[i]);
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+		newCard.addEventListener('click', isTwoCards);
+	}
 }
 
 createCards();
 
-/*if (cardOne === cardTwo) {
-	//|| (cardThree === cardFour) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-};*/
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+		if (this.getAttribute('data-card') === 'king') {
+			this.innerHTML = '<img src="king_hearts.png" alt="King of Hearts" />';
+		} else {
+			this.innerHTML = '<img src="queen_clubs.png" alt="Queen of Clubs" />';
+		}
+		if (cardsInPlay.length === 2) {
+			isMatch(cardsInPlay);
+			cardsInPlay = [];
+	}
+}
 
+
+function isMatch(cards) {
+		if (cards[0] === cards[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		} 
+}
